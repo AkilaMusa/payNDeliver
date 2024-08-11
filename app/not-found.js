d67db88c76e3594fb09ba6e3e9120e5eab9990e0
username-requiredd <pj337167@gmail.com>
2024-08-11 04:17:07 -1100

    added custom 404  page

---
+++ app/not-found.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+export default function ErrorPage() {
+  // const error = useRouteError();
+  // console.error(error);
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="">
+          <div className="flex flex-col items-center">
+            <h1 className="font-bold text-3xl text-blue-600 lg:text-6xl">
+              404
+            </h1>
+
+            <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+              <span className="text-red-500">Oops!</span>
+               {/* Page{" "} */}
+              {/* {error.statusText} */}
+            </h6>
+
+            <p className="mb-4 text-center text-gray-500 md:text-lg">
+           The page you are looking for is hiding from the internet because it’s camera shy{":)"}
+            </p>
+
+            <Link
+              href="/"
+              className="px-5 py-2 rounded-md text-blue-100 bg-blue-600 hover:bg-blue-700"
+            >
+              Go home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
